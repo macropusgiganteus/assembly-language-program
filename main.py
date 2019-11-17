0057fcc2bc4d37eb33882c6d4764a74224f4aee6
macropusgiganteus <35326900+macropusgiganteus@users.noreply.github.com>
2019-11-17 13:12:42 +0700

    fix jalr
    fix jalr
    fix main file to be able to get file input

--- main.py
+++ main.py
@@ -2,34 +2,15 @@
 from assembly import assembly
 from simulator import simulator
 
+input = input("Enter your file name: ")+".txt"
+
 # assembly instruction
 instruction = []
-test = []
-test1 = []
-a = 0
-b = 0
-
-f = open("instruction.txt", "r")
-for x in f:
-    instruction.append(x)
 
 
-t = open("test.txt", "r")
+t = open(input, "r")
 for y in t:
-    test.append(y)
+    instruction.append(y)
 
-d = open("test1.txt", "r")
-for y in d:
-    test1.append(y)
-
-print('\n instruction file \n')
 # main
-#simulator(instruction)
-simulator(test1)
-
-print('\ntest file jalr \n')
-
-for y in test:
-    assembly(y, b)
-    b += 1
-b = 0
+simulator(instruction)
